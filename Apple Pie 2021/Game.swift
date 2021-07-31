9a433f6f382b5a134aab0e3eb43a9916e79a6991
Valeriy Kosyakin <valeradzu@mail.ru>
2021-07-31 11:50:26 +0300

    Added Game model

---
+++ Apple Pie 2021/Game.swift
@@ -0,0 +1,11 @@
+//
+//  Game.swift
+//  Apple Pie 2021
+//
+//  Created by Валера on 27.07.2021.
+//
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- Apple Pie 2021/ViewController.swift
+++ Apple Pie 2021/ViewController.swift
@@ -9,11 +9,123 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var coorectWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    // MARK: - Properties
+    var currentGame: Game!
+    let incorrectMovesAllowed = 7
+    var listOfWords = [
+                       "Александрия",
+                       "Атланта",
+                       "Ахмедабад",
+                       "Багдад",
+                       "Бангалор",
+                       "Бангкок",
+                       "Барселона",
+                       "Белу-Оризонти",
+                       "Богота",
+                       "Буэнос-Айрес",
+                       "Вашингтон",
+                       "Гонконг",
+                       "Гуанчжоу",
+                       "Дакка",
+                       "Даллас",
+                       "Далянь",
+                       "Дар-эс-Салам",
+                       "Дели",
+                       "Джакарта",
+                       "Дунгуань",
+                       "Йоханнесбург",
+                       "Каир",
+                       "Калькутта",
+                       "Карачи",
+                       "Киншаса",
+                       "Куала Лумпур",
+                       "Лагос",
+                       "Лахор",
+                       "Лима",
+                       "Лондон",
+                       "Лос-Анджелес",
+                       "Луанда",
+                       "Мадрид",
+                       "Майами",
+                       "Манила",
+                       "Мехико",
+                       "Москва",
+                       "Мумбаи",
+                       "Нагоя",
+                       "Нанкин",
+                       "Нью-Йорк",
+                       "Осака",
+                       "Париж",
+                       "Пекин",
+                       "Пуна",
+                       "Рио-де-Жанейро",
+                       "Сан-Паулу",
+                       "Санкт-Петербург",
+                       "Сантьяго",
+                       "Сеул",
+                       "Сиань",
+                       "Сингапур",
+                       "Стамбул",
+                       "Сурат",
+                       "Сучжоу",
+                       "Тегеран",
+                       "Токио",
+                       "Торонто",
+                       "Тяньцзинь",
+                       "Ухань",
+                       "Филадельфия",
+                       "Фошань",
+                       "Фукуока",
+                       "Хайдарабад",
+                       "Ханчжоу",
+                       "Харбин",
+                       "Хартум",
+                       "Хошимин",
+                       "Хьюстон",
+                       "Цзинань",
+                       "Циндао",
+                       "Ченнай",
+                       "Чикаго",
+                       "Чунцин",
+                       "Чэнду",
+                       "Шанхай",
+                       "Шэньчжэнь",
+                       "Шэньян",
+                       "Эр-Рияд",
+                       "Янгон",
+          ]
+                      
+  var totalWins = 0
+  var totalLosses = 0
+    
+    // MARK: - Methods
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI(){
+        let movesRemaining = currentGame.incorrectMovesRemaining
+        let image = "Tree\(movesRemaining < 8 ? movesRemaining : 7)"
+        treeImageView.image = UIImage(named: image)
+        scoreLabel.text = "Выигрыши:\(totalWins),Проигрыши:\(totalLosses)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         newRound()
+        }
+
+    // MARK: - IB Actions
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
     }
-
-
+    
 }
 
